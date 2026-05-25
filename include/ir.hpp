3c06a3c71aff8b1315dc03ff874410f83eb81082
misterekkk <kapipiotrowski.kp@gmail.com>
2026-05-26 01:18:22 +0200

    feat: added IR for easier parsing

---
+++ include/ir.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+
+namespace lignum {
+
+    struct TempBinNode {
+        bool is_leaf;
+        double threshold_or_value;
+        int feature_id;
+        int default_dir;
+        std::unique_ptr<TempBinNode> left;
+        std::unique_ptr<TempBinNode> right;
+
+        TempBinNode() : is_leaf(false), threshold_or_value(0.0), feature_id(-1), 
+                        default_dir(0), left(nullptr), right(nullptr) {}
+    };
+
+} // namespace lignum
+
