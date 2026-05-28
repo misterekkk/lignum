e416df08aeeee7148056074407d23488a239ffa2
misterekkk <kapipiotrowski.kp@gmail.com>
2026-05-28 22:14:56 +0200

    fix: changes in parser for xgboost, now works properly and also parse base_score

--- include/parsers.hpp
+++ include/parsers.hpp
@@ -10,7 +10,7 @@ namespace lignum {
     namespace parsers {
 
         std::vector<std::unique_ptr<TempBinNode>> load_lightgbm_json(const std::string& filepath);
-        std::vector<std::unique_ptr<TempBinNode>> load_xgboost_json(const std::string& filepath);
+        std::vector<std::unique_ptr<TempBinNode>> load_xgboost_json(const std::string& filepath, double& base_score);
 
     } // namespace parsers
 
--- src_core/parser_xgboost.cpp
+++ src_core/parser_xgboost.cpp
@@ -18,7 +18,7 @@ namespace lignum {
 
                 if (left_idx == -1) {
                     node->is_leaf = true;
-                    node->threshold_or_value = tree_obj["base_weights"].get_array().at(node_id).get_double();
+                    node->threshold_or_value = tree_obj["split_conditions"].get_array().at(node_id).get_double();
                 } else {
                     node->is_leaf = false;
                     node->threshold_or_value = tree_obj["split_conditions"].get_array().at(node_id).get_double();
@@ -36,11 +36,33 @@ namespace lignum {
         
         } // namespace
 
-        std::vector<std::unique_ptr<TempBinNode>> load_xgboost_json(const std::string& filepath) {
+        std::vector<std::unique_ptr<TempBinNode>> load_xgboost_json(const std::string& filepath, double& out_base_score) {
             simdjson::dom::parser parser;
             std::vector<std::unique_ptr<TempBinNode>> forest;
 
             simdjson::dom::element doc = parser.load(filepath);
+
+            double base_score = 0.0;
+            auto base_score_element = doc["learner"]["learner_model_param"]["base_score"];
+            if (base_score_element.error() == simdjson::SUCCESS) {
+                auto base_score_string = std::string(base_score_element.get_string().value());
+                base_score_string.erase(std::remove(base_score_string.begin(), base_score_string.end(), "]"), base_score_string.end());
+                base_score_string.erase(std::remove(base_score_string.begin(), base_score_string.end(), "["), base_score_string.end());
+
+                base_score = std::stod(base_score_string);
+            }
+
+            auto loss_func_element = doc["learner"]["objective"]["name"];
+            if (loss_func_element.error() == simdjson::SUCCESS) {
+                auto loss_func_string = std::string(loss_func_element.get_string().value());
+                if (loss_func_string == "binary:logistic") {
+                    double eps = 1e-15;
+                    base_score = std::min(std::max(base_score, eps), 1 - eps);
+                    out_base_score = std::log(base_score / (1 - base_score));
+                } else if (loss_func_string == "reg:squarederror" || loss_func_string == "reg:pseudohubererror" || loss_func_string == "reg:linear") {
+                    out_base_score = base_score;
+                }
+            }
 
             simdjson::dom::array trees = doc["learner"]["gradient_booster"]["model"]["trees"].get_array();
 
